d6209997aa18dbbeecf31aac6ac14c7b327bac86
janr0599 <javiernr0599@gmail.com>
2024-11-09 18:07:42 -0400

    solve Cors issue

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import morgan from "morgan";
-import { connectDB } from "./config.ts/db";
+import { connectDB } from "./config/db";
 import ticketsRoutes from "./routes/ticketsRoutes";
+import { corsConfig } from "./config/cors";
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+
+app.use(cors(corsConfig));
 
 // Log requests to console
 app.use(morgan("dev"));
